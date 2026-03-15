cad30ffd59baa8c8d3de21ec7250b6672ee4c137
shresthasagun3 <ssagun336@gmail.com>
2026-03-15 01:49:43 -0600

    email replaced in login.js

--- src/components/login.js
+++ src/components/login.js
@@ -27,10 +27,10 @@ function Login() {
     <div className="login-container">
         <Form onSubmit={login} className='login-form bg-dark text-light p-4 rounded'> {/* Use onSubmit for the form */}
         <Form.Group controlId="username" className="mb-3">
-            <Form.Label>Email</Form.Label>
+            <Form.Label>Username</Form.Label>
             <Form.Control
-            type="email"
-            placeholder="Enter email"
+            type="text"
+            placeholder="Enter username"
             autoComplete="username"
             value={details.username}
             onChange={updateDetails}
