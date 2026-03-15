37b2b7c83a1b24d354bd840868eb132554760924
shresthasagun3 <ssagun336@gmail.com>
2026-03-15 01:58:37 -0600

    modifications to register.js

--- src/components/register.js
+++ src/components/register.js
@@ -19,28 +19,29 @@ function Register() {
         });
     };
 
-    const register = () => {
+    const register = (event) => {
+        event.preventDefault();
         dispatch(submitRegister(details));
     };
 
     return (
         <div className="register-container">
-            <Form className='register-form bg-dark text-light p-4 rounded'>
-                <Form.Group controlId="name">
+            <Form onSubmit={register} className='register-form bg-dark text-light p-4 rounded'>
+                <Form.Group controlId="name" className="mb-3">
                     <Form.Label>Name</Form.Label>
                     <Form.Control onChange={updateDetails} value={details.name} type="text" placeholder="Name" />
                 </Form.Group>
 
-                <Form.Group controlId="username">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control onChange={updateDetails} value={details.username} autoComplete="username" type="email" placeholder="Enter email" />
+                <Form.Group controlId="username" className="mb-3">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control onChange={updateDetails} value={details.username} autoComplete="username" type="text" placeholder="Enter username" />
                 </Form.Group>
 
-                <Form.Group controlId="password">
+                <Form.Group controlId="password" className="mb-3">
                     <Form.Label>Password</Form.Label>
                     <Form.Control onChange={updateDetails} value={details.password} autoComplete="current-password" type="password" placeholder="Password" />
                 </Form.Group>
-                <Button onClick={register}>Register</Button>
+                <Button type="submit">Register</Button>
             </Form>
         </div>
     );
