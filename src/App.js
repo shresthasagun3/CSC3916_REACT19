dc0ef649f524c13246fc535ed5fff34726df1434
shresthasagun3 <ssagun336@gmail.com>
2026-04-19 04:10:04 -0600

    update to app.js code

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import MovieHeader from './components/movieheader';
 import MovieList from './components/movielist';
 import Movie from './components/movie';
 import Authentication from './components/authentication';
-import {HashRouter, Routes,  Route} from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
+  const token = localStorage.getItem('token');
+
   return (
     <div className="App">
-      <HashRouter> {/* The Router component */}
+      <HashRouter>
         <MovieHeader />
         <Routes>
-          <Route path="/" element={<MovieList />} />
-          <Route path="/movielist" element={<MovieList />}/>
-          <Route path="/movie/:movieId" element={<Movie />}/>
-          <Route path="/signin" element={<Authentication />}/>
-          <Route path="/signup" element={<Authentication />}/>
-          {/*... other routes */}
+          <Route path="/" element={token ? <MovieList /> : <Navigate to="/signin" />} />
+          <Route path="/movielist" element={token ? <MovieList /> : <Navigate to="/signin" />} />
+          <Route path="/movie/:movieId" element={token ? <Movie /> : <Navigate to="/signin" />} />
+          <Route path="/signin" element={<Authentication />} />
+          <Route path="/signup" element={<Authentication />} />
         </Routes>
       </HashRouter>
     </div>
