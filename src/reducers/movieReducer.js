37e0cbcea6d6fdd412219a6e80d4c15a192d9726
shresthasagun3 <ssagun336@gmail.com>
2026-04-19 04:27:08 -0600

    modifying fetch movies

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -10,14 +10,14 @@ const movieReducer = (state = initialState, action) => {
 
       switch(action.type) {
             case constants.FETCH_MOVIES:
-                  updated['movies'] = action.movies.movies;
-                  updated['selectedMovie'] = action.movies.movies[0];
+                  updated['movies'] = action.movies;
+                  updated['selectedMovie'] = action.movies[0];
                   return updated;
             case constants.SET_MOVIE:
                   updated['selectedMovie'] = action.selectedMovie;
                   return updated;
             case constants.FETCH_MOVIE:
-                  updated['selectedMovie'] = action.selectedMovie.movie;  
+                  updated['selectedMovie'] = action.selectedMovie;  
                   return updated;
             default:
                   return state;
